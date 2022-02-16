fb81bd9e8b69437e06bb416e768eea13277ab102
creepozoid <creepozoidberg@gmail.com>
2022-02-15 19:05:18 -0800

    initial commit

---
+++ repetition02.py
@@ -0,0 +1,6 @@
+ 
+sum = 0
+for i in range(1,6):  
+    n = int(input("Enter the number ")) 
+    sum = n+sum  
+    print(sum) 
---
+++ repetition03.py
@@ -0,0 +1,8 @@
+sum = 0
+i = 0
+while i < 5:  
+    n = int(input("Enter the number ")) 
+    sum = n+sum  
+    i = i + 1
+    print(sum) 
+    print (i)
