b88afbae411506330605a00a7442eb741ed83b52
Kayw Lee <kwooler@gmail.com>
2022-03-22 17:08:10 -0700

    7-8

--- 7-8.py
+++ 7-8.py
@@ -29,3 +29,30 @@ if len(list1) > len(list2):
             for m in range(x):
                 list3.append(list1[len(list2) + m])
 print (list3)
+
+# Hope you check out the below code.
+# N = int(input())
+
+# num1 = [0] * N
+# for i in range(N):
+# 	num1[i] = int(input())
+# print (num1)
+
+# M = int(input())
+# num2 = [0] * M
+# for i in range(M):
+# 	num2[i] = int(input())
+# print (num2)
+
+# minlen = N if N < M else M
+# num3 = [0] * (minlen * 2)
+# for i in range(minlen):
+# 	num3[2*i] = num1[i]
+# 	num3[2*i+1] = num2[i] 
+
+# if N == minlen:
+# 	num3 = num3 + num2[minlen:]
+# else:
+# 	num3 = num3 + num1[minlen:]
+
+# print (num3)
