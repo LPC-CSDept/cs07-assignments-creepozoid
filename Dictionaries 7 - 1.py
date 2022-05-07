e21c6bca9342ac45564ae421a1c896475e3bf649
creepozoid <creepozoidberg@gmail.com>
2022-05-07 13:30:54 -0700

    4

--- Dictionaries 7 - 1.py
+++ Dictionaries 7 - 1.py
@@ -6,4 +6,16 @@ import xlrd
 
 xls = 'student.xls'
 work_book = xlrd.open_workbook(xls)
-print (xls)
+worksheet = work_book.sheet_by_index(0)
+#print (xls)
+header = []
+id = []
+names = []
+scores = []
+
+for i in range(worksheet.ncols):
+    if worksheet.row_values(0)[i] != '':
+        header.append(worksheet.row_values(0)[i])
+    else:
+        continue
+print ("header =", header)
