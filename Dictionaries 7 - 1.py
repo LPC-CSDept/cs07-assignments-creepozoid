011e80bd39f62208dd0bf7bbb0af4963283c40a7
creepozoid <creepozoidberg@gmail.com>
2022-05-07 12:58:37 -0700

    3

--- Dictionaries 7 - 1.py
+++ Dictionaries 7 - 1.py
@@ -1,10 +1,9 @@
-
-import requests
+#import requests
 import xlrd
-import urllib
+#import urllib
+#import os
+#print (os.getcwd)
 
-link = 'https://github.com/Kwooley/CS07/blob/main/Chap09/student.xls'
-file_name, headers = urllib.request.urlretrieve(link)
-print (file_name)
-workbook = xlrd.open_workbook(file_name)
-print (workbook)
+xls = 'student.xls'
+work_book = xlrd.open_workbook(xls)
+print (xls)
