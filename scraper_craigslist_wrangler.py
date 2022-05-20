1acf9a3e6ab93398a816745b3952d55c9fa85712
creepozoid <creepozoidberg@gmail.com>
2022-05-19 23:39:55 -0700

    second commit

--- scraper_craigslist_wrangler.py
+++ scraper_craigslist_wrangler.py
@@ -1,5 +1,7 @@
-#import sys
-import re
+# This scraper get from craigslist listing of all 2004 -2006 jeep wranglers 
+# that are for sale in SF Bay Area and create a dictionary 
+# where key is title of an ad and value is the price of the corresponding vehicle
+
 import requests
 from bs4 import BeautifulSoup as soup
 page = requests.get("https://sfbay.craigslist.org/search/cta?purveyor=owner&auto_make_model=jeep+wrangler&min_auto_year=2004&max_auto_year=2006")
@@ -7,18 +9,15 @@ bsobj = soup(page.content,'lxml')
 links = []
 for link in bsobj.findAll('li',{'class':'result-row'}):
     links.append(link.a['href'])
-title = {}
+dict01 = {}
 for link in links:
     page = requests.get(link)
     bsobj = soup(page.content,'lxml')
     print(bsobj.findAll('h1')[0].text.strip())
-#    title.append(bsobj.findAll('h1')[0].text.strip()) #иуи надо переделать. перед этим разьбить строки на элементы и из них налепить кей-валбю пар. из уже и добавлять в {}
     s = (bsobj.findAll('h1')[0].text.strip())
     x = (s.split(" - "))[0]
-#    print(x)
     m = (s.split("-"))[1]
     y = (m.split("("))[0]
-#    print(m)
-    title[x] = y
-
-print(title)
+    dict01[x] = y
+print()
+print(dict01)
